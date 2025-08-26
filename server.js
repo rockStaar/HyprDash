6ad87c3615e8fa0072658b8d450d6237c7fb3d27
rockStaar <swarmanaswi131513@gmail.com>
2025-08-26 23:10:55 +0530

    Also tried this project using websockets (jus for the sake of experimentation). For this scale, I personally did not saw much difference between in the frontend level, but as the project progresses I received advice that its better to use socket.io as it reconnects automatically which websocket fails to do so

--- server.js
+++ server.js
@@ -1,30 +1,40 @@
-const express = require("express");
-const http = require("http");
-const { Server } = require("socket.io");
-const metricsSocket = require("./sockets/metricsSocket.js");
+const express = require("express");    // import express
+const http = require("http")
+const WebSocket = require('ws')
+const metrics = require("./metrics/index.js");    // import metrics functions
 
-const app = express();
-const server = http.createServer(app); // Create HTTP server
-const io = new Server(server, {
-  cors: {
-    origin: "*", // later restrict to frontend URL
-  },
+const app = express();    // create express app
+const server = http.createServer(app);
+
+// WebSocket server
+const wss = new WebSocket.Server({ server });
+
+wss.on("connection", (ws) => {
+  console.log("Client connected via WebSocket");
+
+  // Send metrics every 2 seconds
+  const interval = setInterval(async () => {
+    try {
+      const data = {
+        cpu: await metrics.cpuUsage(),
+        disk: await metrics.diskUsage(),
+        memory: await metrics.checkMemory(),
+        network: await metrics.getNetworkUsage(),
+      };
+
+      ws.send(JSON.stringify(data));
+    } catch (err) {
+      console.error("Error collecting metrics:", err);
+    }
+  }, 2000);
+
+  ws.on("close", () => {
+    console.log("Client disconnected");
+    clearInterval(interval);
+  });
 });
 
 const PORT = process.env.PORT || 5000;
-
-// Basic test route
-app.get("/", (req, res) => {
-  res.send("HyprDash backend running!");
-});
-
-// Setup socket connections
-io.on("connection", (socket) => {
-  console.log("Client connected:", socket.id);
-  metricsSocket(socket); // attach metrics logic for this client
-});
-
-// Start server
 server.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
